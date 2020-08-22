51be9e95d5fbe3a526fe963af6042ae56ada0a2d
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2020-08-22 08:22:58 +0200

    more concise solution without any imports

--- isogram/src/main/java/IsogramChecker.java
+++ isogram/src/main/java/IsogramChecker.java
@@ -1,17 +1,9 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        List<Character> charactersToConsider = IntStream.range(0, phrase.length())
-                .mapToObj(i -> phrase.toLowerCase().charAt(i))
-                .filter(character -> character != ' ' && character != '-')
-                .collect(Collectors.toList());
-
-        return charactersToConsider.size() == new HashSet<>(charactersToConsider).size();
+        final String temp = phrase.toLowerCase().replace("-", "").replace(" ", "");
+        return temp.chars().distinct().count() == temp.length();
     }
 
 }
